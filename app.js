f0d01384c6d4ae17362b4616f2e7371fc8cfbff8
Prerit Mahajan <prerit@Prerits-MacBook-Pro.local>
2021-07-02 07:21:41 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Sweet Caroline'));
+app.get('/', (req, res) => res.send('Sunshine of Your Love'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
